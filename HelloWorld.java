ebfc4bbcaf681f12dce41a61445708c33e53bd40
mitie150 <ofv0602@gmail.com>
2022-09-08 21:12:49 -0400

    Second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,37 +3,25 @@ class HelloWorld {
     public static void main(String args[]) {
 
         int[][] matrix = {
-                {1, 1, 1, 1, 3},
-                {9, 8, 9, 1, 9},
-                {7, 1, 1, 1, 9},
-                {2, 9, 2, 9, 1},
-                {2, 8, 3, 9, 7}
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
         };
-        
+
         System.out.println(minimumPathSum(matrix));
     }
 
     public static int minimumPathSum(int[][] matrix) {
-
-        int distance = 0;
-        int currentX = 0;
-        int currentY = 0;
         int width = matrix.length;
         int height = matrix[0].length;
-        int currentBest = Integer.MAX_VALUE;
-        int currentDistance = 0;
-        int[][] tracker = new int[width][height];
-        
-        findMinimum(currentX, currentY, matrix, tracker, currentBest, currentDistance);
 
-        return distance;
+        return findMinimum(0, 0, matrix, new int[width][height], Integer.MAX_VALUE, 0);
     }
 
     public static int findMinimum(int currentX, int currentY, int[][] matrix, int[][] tracker, int currentBest, int currentDistance) {
 
         // Problem 1: It doesn't work
         // Problem 2: Set tracker back to zero at some point
-        // Problem 3: Put this in git
         // Problem 4: Check this works for rectangular matrices
 
         // Update state
@@ -43,9 +31,16 @@ class HelloWorld {
         int width = matrix.length;
         int height = matrix[0].length;
         if (currentX == width - 1 && currentY == height - 1){
+            // Print everything (currentDistance, currentBest, tracker)
+            System.out.println("currentDistance: " + currentDistance + "currentBest: " + currentBest);
+
+            System.out.println(tracker[0][0] + " " + tracker[0][1] + " " + tracker[0][2]);
+            System.out.println(tracker[1][0] + " " + tracker[1][1] + " " + tracker[1][2]);
+            System.out.println(tracker[2][0] + " " + tracker[2][1] + " " + tracker[2][2]);
+
             return currentDistance < currentBest ? currentDistance : currentBest;
         }
-        
+
         boolean canGoRight = (currentX != width - 1) && tracker[currentX + 1][currentY] == 0;
         boolean canGoLeft = (currentX != 0) && tracker[currentX - 1][currentY] == 0;
         boolean canGoUp = (currentY != 0) && tracker[currentX][currentY - 1] == 0;
@@ -58,14 +53,16 @@ class HelloWorld {
         if (canGoLeft){
             currentBest = findMinimum(currentX - 1, currentY, matrix, tracker, currentBest, currentDistance);
         }
+
         if (canGoUp){
             currentBest = findMinimum(currentX, currentY - 1, matrix, tracker, currentBest, currentDistance);
         }
+
         if (canGoDown) {
             currentBest = findMinimum(currentX, currentY + 1, matrix, tracker, currentBest, currentDistance);
         }
 
-        tracker = new int[width][height];
+        tracker[currentX][currentY] = 0;
 
         return currentBest;
     }
